ef4133fa1c1fb8154841079c04ab185b0b13863d
sala <saladinkzn@gmail.com>
2014-12-02 00:06:54 +0300

    added java resources test

--- plugins/src/test/java/ru/shadam/initializer/plugin/java/JavaPluginTest.java
+++ plugins/src/test/java/ru/shadam/initializer/plugin/java/JavaPluginTest.java
@@ -2,13 +2,16 @@ package ru.shadam.initializer.plugin.java;
 
 import freemarker.template.Configuration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ru.shadam.initializer.archive.File;
 import ru.shadam.initializer.plugin.gradle.GradlePlugin;
 import ru.shadam.initializer.plugin.java.config.JavaClass;
+import ru.shadam.initializer.plugin.jvm.config.Resource;
 import ru.shadam.initializer.project.Project;
 import ru.shadam.initializer.renderer.FreemarkerRenderer;
 
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,15 +20,20 @@ import java.util.Set;
  * @author sala
  */
 public class JavaPluginTest {
-    @Test
-    public void testJavaPlugin() {
+    private FreemarkerRenderer freemarkerRenderer;
+
+    @Before
+    public void initTest() {
         final Configuration configuration = new Configuration();
         configuration.setDefaultEncoding("UTF-8");
         configuration.setOutputEncoding("UTF-8");
         configuration.setClassForTemplateLoading(JavaPluginTest.class, "/");
         //
-        final FreemarkerRenderer freemarkerRenderer = new FreemarkerRenderer(configuration);
-        //
+        freemarkerRenderer = new FreemarkerRenderer(configuration);
+    }
+
+    @Test
+    public void testJavaPlugin() {
         final Project project = new Project(freemarkerRenderer);
         //
         project.registerPlugin(new GradlePlugin());
@@ -48,5 +56,30 @@ public class JavaPluginTest {
         //
         Assert.assertTrue(fileNames.contains("build.gradle"));
         Assert.assertTrue(fileNames.contains("src/main/java/org/example/Program.java"));
+    }
+
+    @Test
+    public void testJavaResourcePlugin() {
+        final Project project = new Project(freemarkerRenderer);
+        //
+        project.registerPlugins(new GradlePlugin(), new JavaPlugin());
+        //
+        final JavaConfig config = project.getConfig(JavaPlugin.JAVA_CONFIG_KEY);
+        //
+        config.getClasses()
+                .add(new JavaClass("org.example", "Program", "program.java.ftl"));
+        config.getResources()
+                .add(new Resource("", "application.properties", "application.properties.ftl"));
+        //
+        final List<File> files = project.execute();
+        final Set<String> fileNames = new HashSet<>();
+        //
+        for (File file: files ) {
+            fileNames.add(file.getName());
+        }
+        //
+        Assert.assertTrue(fileNames.contains("build.gradle"));
+        Assert.assertTrue(fileNames.contains(Paths.get("src", "main", "java", "org", "example", "Program.java").toString()));
+        Assert.assertTrue(fileNames.contains(Paths.get("src", "main", "resources", "application.properties").toString()));
     }
 }
